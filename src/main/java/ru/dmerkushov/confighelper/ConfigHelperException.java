ac2e2c3cdc5d6df901cf69ab876909cd1a9fd2d3
shandr <shandr@shandr.site>
2013-11-29 19:49:04 +0400

    Prepared for Maven Central

--- src/main/java/ru/dmerkushov/confighelper/ConfigHelperException.java
+++ src/main/java/ru/dmerkushov/confighelper/ConfigHelperException.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ru.dmerkushov.confighelper;
 
 /**
@@ -11,37 +10,46 @@ package ru.dmerkushov.confighelper;
  */
 public class ConfigHelperException extends Exception {
 
-    /**
-     * Creates a new instance of <code>ConfigHelperException</code> without detail message.
-     */
-    public ConfigHelperException() {
-    }
+	/**
+	 * Creates a new instance of
+	 * <code>ConfigHelperException</code> without detail message.
+	 */
+	public ConfigHelperException () {
+	}
 
+	/**
+	 * Constructs an instance of
+	 * <code>ConfigHelperException</code> with the specified detail message.
+	 *
+	 * @param msg the detail message.
+	 */
+	public ConfigHelperException (String msg) {
+		super (msg);
+	}
 
-    /**
-     * Constructs an instance of <code>ConfigHelperException</code> with the specified detail message.
-     * @param msg the detail message.
-     */
-    public ConfigHelperException(String msg) {
-        super(msg);
-    }
+	/**
+	 * Constructs an instance of
+	 * <code>ConfigHelperException</code> with the specified cause.
+	 *
+	 * @param cause the cause (which is saved for later retrieval by the
+	 * Exception.getCause() method). (A null value is permitted, and indicates
+	 * that the cause is nonexistent or unknown.)
+	 */
+	public ConfigHelperException (Throwable cause) {
+		super (cause);
+	}
 
-
-    /**
-     * Constructs an instance of <code>ConfigHelperException</code> with the specified cause.
-	 * @param cause the cause (which is saved for later retrieval by the Exception.getCause() method). (A null value is permitted, and indicates that the cause is nonexistent or unknown.)
-     */
-    public ConfigHelperException(Throwable cause) {
-        super(cause);
-    }
-
-
-    /**
-     * Constructs an instance of <code>ConfigHelperException</code> with the specified detail message and cause.
-     * @param msg the detail message.
-	 * @param cause the cause (which is saved for later retrieval by the Exception.getCause() method). (A null value is permitted, and indicates that the cause is nonexistent or unknown.)
-     */
-    public ConfigHelperException(String msg, Throwable cause) {
-        super(msg, cause);
-    }
+	/**
+	 * Constructs an instance of
+	 * <code>ConfigHelperException</code> with the specified detail message and
+	 * cause.
+	 *
+	 * @param msg the detail message.
+	 * @param cause the cause (which is saved for later retrieval by the
+	 * Exception.getCause() method). (A null value is permitted, and indicates
+	 * that the cause is nonexistent or unknown.)
+	 */
+	public ConfigHelperException (String msg, Throwable cause) {
+		super (msg, cause);
+	}
 }
